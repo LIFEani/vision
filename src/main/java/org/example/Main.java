187b915ee66f38e08f9ebce57a2d111a80226047
LIFEani <llqlljw@126.com>
2024-08-02 14:12:12 +0800

    test1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,22 @@ import com.alibaba.fastjson.JSONObject;
 
 import java.time.Instant;
 import java.util.*;
-
+import java.util.stream.Collectors;
 
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
 
-        long a = Instant.now().toEpochMilli();
-        Thread.sleep(1000L);
-        long b = Instant.now().toEpochMilli();
-
-        System.out.println( b-a  );
-        System.out.println("..assdsddddd..");
-
+        List<String> list =new ArrayList<String>();
+        list.add("a");
+        list.add("b");
+        Set<String> a = list.stream().filter(t-> "a".equals(t)).filter(t-> "b".equals(t)).collect(Collectors.toSet());
+        if (a.size()!=0){
+            System.out.println("null");
+        } else {
+            System.out.println("no");
+        }
 
 
     }
