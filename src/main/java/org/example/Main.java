edd7b1a1bdc9a1c4204482e14588803049e2fa1d
LIFEani <llqlljw@126.com>
2024-09-05 13:55:10 +0800

    json

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import java.time.Instant;
@@ -11,7 +12,12 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        JSONObject jsonObject = new JSONObject();
 
+        jsonObject.put("name", "John");
+        jsonObject.put("age", 30);
+
+        System.out.println(JSON.toJSONString(jsonObject));
 
 
 
