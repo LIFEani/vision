a555ee078b9a55086ce42d90bafad6261bb09a8f
LIFEani <llqlljw@126.com>
2024-10-31 19:58:37 +0800

    student-Stream
    (cherry picked from commit 3e4db826e6d190e51b5ff3b87dbe3bd5f19fe69a)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,27 @@
 package org.example;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Main {
 
     public static void main(String[] args){
+        List<Student> studentList = Arrays.asList(
+                new Student("A", 10, "At-txt"),
+                new Student("B", 34, "Bt-txt"),
+                new Student("C", 24, "Ct-txt"),
+                new Student("D", 42, "Dt-txt"),
+                new Student("E", 15, "Et-txt"),
+                new Student("F", 52, "Ft-txt")
+        );
+        studentList.stream().flatMap(p -> Stream.of(p.getName(), p.getText())).forEach(System.out::println);
 
-        Calendar t1 = Calendar.getInstance();
-        t1.setTime(new Date());
-        //30min前
-        t1.add(Calendar.MINUTE, -30);
 
-        Calendar t2 = Calendar.getInstance();
-        t2.setTime(new Date());
-
-        System.out.println(t1.getTime());
-        System.out.println(t2.getTime());
     }
 }
 
