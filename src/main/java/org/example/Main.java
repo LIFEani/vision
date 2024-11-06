2edddab4374a4e2b1400a10ee576d5d434a90b77
LIFEani <llqlljw@126.com>
2024-11-06 13:46:11 +0800

    student-parseArray自定义

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 import java.util.*;
@@ -11,18 +12,25 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args){
-        List<Student> studentList = Arrays.asList(
-                new Student("A", 10, "At-txt"),
-                new Student("B", 34, "Bt-txt"),
-                new Student("C", 24, "Ct-txt"),
-                new Student("D", 42, "Dt-txt"),
-                new Student("E", 15, "Et-txt"),
-                new Student("F", 52, "Ft-txt")
-        );
-        studentList.stream().flatMap(p -> Stream.of(p.getName(), p.getText())).forEach(System.out::println);
 
+           //T1();
+            T2();
 
     }
+    public static void T1(){
+        List<Student> studentList = Arrays.asList(
+                new Student("A", 10, "At-txt",new Date()),
+                new Student("B", 34, "Bt-txt",new Date())
+        );
+        String jsonString = JSON.toJSONString(studentList);
+        System.out.println(jsonString);
+    }
+    public static void T2(){
+        String a = "[{\"age\":10,\"birthday\":\"null\",\"name\":\"A\",\"text\":\"At-txt\"},{\"age\":34,\"birthday\":\"2024-11-06 11:29:40\",\"name\":\"B\",\"text\":\"Bt-txt\"}]";
+        List<Student> students = JSON.parseArray(a, Student.class);
+        System.out.println(JSON.toJSONString(students));
+    }
+
 }
 
 
