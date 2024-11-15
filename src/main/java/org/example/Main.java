c97e1f74bb73558c01b2e94ccf6acec16ba99ff0
LIFEani <llqlljw@126.com>
2024-11-15 15:05:48 +0800

    导出excel

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,54 @@
 package org.example;
 
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+
+import com.alibaba.excel.EasyExcel;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args){
         T1();
     }
     public static void T1() {
-        System.out.println("aaaaaaa");
+        List<Student> studentList = Arrays.asList(
+                new Student("A", 10, "A-os","A-pas","At-txt",new Date()),
+                new Student("B", 34, "Bt-os","B-pas","Bt-txt",new Date()),
+                new Student("C", 24, "Ct-os","C-pas","Ct-txt",new Date()),
+                new Student("D", 42, "Dt-os","D-pas","Dt-txt",new Date()),
+                new Student("E", 15, "Et-os","E-pas","Et-txt",new Date()),
+                new Student("F", 52, "Ft-os","F-pas","Ft-txt",new Date())
+        );
+        T2(studentList, "src/main/resources/test.xls");
     }
-    public static void T2() {
+    public static <T> void T2(List<T> dataList, String fileName) {
+//        // 注意：在数据量不大的情况下可以使用（5000以内，具体也要看实际情况），数据量大参照方法2，重复多次写入。
+//        // 方法1: 一次写到同一个sheet
+//        EasyExcel.write(fileName)
+//                .head(Student.class)
+//                .sheet()
+//                .doWrite(dataList);
 
+        // 方法2: 多次写到同多个sheet
+        try (ExcelWriter excelWriter = EasyExcel.write(fileName, Student.class).build()) {
+            WriteSheet writeSheet1 = EasyExcel.writerSheet("sheet01").build();
+            WriteSheet writeSheet2 = EasyExcel.writerSheet("sheet02").build();
+
+            int batchSize = 2;
+            for (int i = 0; i < dataList.size(); i+= batchSize) {
+                List<T> demoDataList = dataList.subList(i, Math.min(i + batchSize, dataList.size()));
+                excelWriter.write(demoDataList, writeSheet1);
+            }
+            for (int i = 0; i < dataList.size(); i+= batchSize) {
+                List<T> demoDataList = dataList.subList(i, Math.min(i + batchSize, dataList.size()));
+                excelWriter.write(demoDataList, writeSheet2);
+            }
+        }
     }
-
 }
 
 
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,18 +1,36 @@
 package org.example;
 
 
-import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.annotation.write.style.*;
+import com.alibaba.excel.enums.poi.FillPatternTypeEnum;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
+@ColumnWidth(20)   // 列宽，也可以针对每列使用
+@HeadRowHeight(25) // 表头行高
+@ContentRowHeight(15) // 内容行高
+// 头背景设置成红色 IndexedColors.RED.getIndex()
+@HeadStyle(fillPatternType = FillPatternTypeEnum.SOLID_FOREGROUND, fillForegroundColor = 10)
 public class Student {
-    private String name;
+    @ExcelProperty(value = {"主标题","姓名"}, index = 0)
+    @ContentStyle(fillPatternType = FillPatternTypeEnum.SOLID_FOREGROUND, fillForegroundColor = 40)
+    private String name;//设置表头名, {}, 有一个以上的表头信息时，它会自动合并
+    @ExcelProperty(value = {"主标题","年龄"}, index = 1)
     private int age;
+    @ColumnWidth(30)   // 列宽
+    @ExcelProperty(value = {"主标题","账号"}, index = 2)
+    private String account;
+    @ExcelIgnore
+    private String password;//@ExcelIgnore注解 忽略这个字段
+    @ExcelProperty(value = {"主标题", "新增", "备注"}, index = 4)
     private String text;
-    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat("yyyy年MM月dd日HH时mm分ss秒")
+    @ExcelProperty(value = "日期", index = 5)
+    @ColumnWidth(30)   // 列宽
     private Date birthday;
 
     public Student(String name, int age, String text) {
@@ -20,45 +38,43 @@ public class Student {
         this.age = age;
         this.text = text;
     }
-
     public Student(String name, int age, String text, Date birthday) {
         this.name = name;
         this.age = age;
         this.text = text;
         this.birthday = birthday;
     }
-
+    public Student(String name, int age, String account, String password, String text, Date birthday) {
+        this.name = name;
+        this.age = age;
+        this.account = account;
+        this.password = password;
+        this.text = text;
+        this.birthday = birthday;
+    }
     public Student() {
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public String getText() {
         return text;
     }
-
     public void setText(String text) {
         this.text = text;
     }
-
     public Date getBirthday() {
         return birthday;
     }
-
     public void setBirthday(String dateStr) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -66,6 +82,21 @@ public class Student {
         } catch (Exception e) {
             this.birthday = null;
         }
+    }
+    public String getAccount() {
+        return account;
+    }
+    public void setAccount(String account) {
+        this.account = account;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 }
 
